5632be00f5d66018ffb397e55911b913e6997694
Amaymani <amaytripathi19@gmail.com>
2025-01-26 16:30:06 +0530

    Portfolio button fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import  navLinks  from "@/utils/navLinks"
 import Logo from "@/components/logo"
+import Link from "next/link";
 
 export default function BackgroundBeamsDemo() {
   const [mounted, setMounted] = useState(false);
@@ -44,7 +45,7 @@ export default function BackgroundBeamsDemo() {
         </p>
 
         <div className="flex justify-center mt-5">
-          <button className=" bg-slate-800  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+          <Link href={"/portfolio"} className=" bg-slate-800  no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
             <span className="absolute inset-0 overflow-hidden rounded-full">
               <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
             </span>
@@ -69,7 +70,7 @@ export default function BackgroundBeamsDemo() {
               </svg>
             </div>
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-          </button>
+          </Link>
         </div>
 
       </div>
